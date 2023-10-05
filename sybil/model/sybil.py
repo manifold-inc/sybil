c8dee275c12265e42fa0aaf20178e99ef9e3f695
Carro <robertmyers@hey.com>
2023-10-05 14:01:10 -0500

    adding model

---
+++ sybil/model/sybil.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+from imagebind.models import imagebind_model
+
+class Sybil(nn.Module):
+    def __init__(self, config):
+        super(Sybil, self).__init__()
+        self.config = config
+
+        imagebind_ckpt = config['imagebind_ckpt'] if 'imagebind_ckpt' in config else None
+
+        assert imagebind_ckpt is not None, 'imagebind_ckpt is not provided'
+
+        # initalize the visual encoder ( imagebind )
+        self.visual_encoder, self.visual_hidden_size = imagebind_model(config)
+        for name, param in self.visual_encoder.named_parameters():
+            param.requires_grad = False
+        self.visual_encoder.eval()
+        print('Visual encoder initialized.')
+
+    def forward(self, batch):
+        return batch
---
+++ train.py
@@ -0,0 +1,20 @@
+import os
+
+from sybil.model import Sybil
+
+
+
+# check if the imagebind model is available at ./ckpts/imagebind/imagebind_huge.pth if not, download it from https://dl.fbaipublicfiles.com/imagebind/imagebind_huge.pth
+
+if not os.path.exists('./ckpts/imagebind/imagebind_huge.pth'):
+    os.system('mkdir -p ./ckpts/imagebind')
+    os.system('wget -O ./ckpts/imagebind/imagebind_huge.pth https://dl.fbaipublicfiles.com/imagebind/imagebind_huge.pth')
+
+imagebind_ckpt = './ckpts/imagebind/imagebind_huge.pth'
+
+
+config = {
+    'imagebind_ckpt': imagebind_ckpt,
+}
+
+sybil = Sybil(config)
