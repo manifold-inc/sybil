dc1ab360608ad46c4b1bedabe3591aa00eb7b042
Carro <robertmyers@hey.com>
2023-10-05 14:20:27 -0500

    adding model

--- sybil/model/engine.py
+++ sybil/model/engine.py
@@ -8,12 +8,16 @@ class Engine(nn.Module):
         super(Engine, self).__init__()
         self.config = config
 
-
+        
         # initalize the visual encoder ( imagebind )
-        self.visual_encoder, self.visual_hidden_size = imagebind_model.imagebind_huge(pretrained=True)
+        self.visual_hidden_size = 1024
+        self.visual_encoder = imagebind_model.imagebind_huge(pretrained=True)
+
+        # freeze the visual encoder
         for name, param in self.visual_encoder.named_parameters():
             param.requires_grad = False
         self.visual_encoder.eval()
+        
         print('Visual encoder initialized.')
 
     def forward(self, batch):
