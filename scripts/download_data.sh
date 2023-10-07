1fb2bf58b9e18ca538f68cf20333e71e416dfab1
Carro <robertmyers@hey.com>
2023-10-07 14:52:23 -0500

    inital download script

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -7,6 +7,10 @@ mkdir cc3m
 cd cc3m
 curl -O https://storage.googleapis.com/gcc-data/Train/GCC-training.tsv
 
+echo -e "caption\turl\n" > header.tsv
+cat header.tsv GCC-training.tsv > temp.tsv && mv temp.tsv GCC-training.tsv
+rm header.tsv
+
 img2dataset --url_list GCC-training.tsv --input_format "tsv" --url_col "url" --caption_col "caption" --output_format webdataset --output_folder cc3m --processes_count 16 --thread_count 64 --image_size 256 --enable_wandb True
 cd ../
 
