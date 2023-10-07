45a0bae87312a5c2fe1b206193a499625958b0b9
Carro <robertmyers@hey.com>
2023-10-07 14:38:28 -0500

    inital download script

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -2,8 +2,10 @@
 cd data
 cd T-X_pair_data/
 # download cc3m
-
+cd cc3m
+wget -nc https://storage.cloud.google.com/gcc-data/Train/GCC-training.tsv
 img2dataset --url_list Train_GCC-training.tsv --input_format "tsv" --url_col "url" --caption_col "caption" --output_format webdataset --output_folder cc3m --processes_count 16 --thread_count 64 --image_size 256 --enable_wandb True
+cd ../
 
 # webvid download
 # wget -nc http://www.robots.ox.ac.uk/~maxbain/webvid/results_2M_train.csv
