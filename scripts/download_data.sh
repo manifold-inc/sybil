6d1dacc3a0e0e832f61ec876f4dd46b5ac358bbe
Carro <robertmyers@hey.com>
2023-10-07 14:36:19 -0500

    inital download script

---
+++ scripts/download_data.sh
@@ -0,0 +1,10 @@
+# get into correct directory for multi modality data
+cd data
+cd T-X_pair_data/
+# download cc3m
+
+img2dataset --url_list Train_GCC-training.tsv --input_format "tsv" --url_col "url" --caption_col "caption" --output_format webdataset --output_folder cc3m --processes_count 16 --thread_count 64 --image_size 256 --enable_wandb True
+
+# webvid download
+# wget -nc http://www.robots.ox.ac.uk/~maxbain/webvid/results_2M_train.csv
+
